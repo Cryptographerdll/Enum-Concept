4d2829c7ca52959ca2162215fb9ad2aebf5a4e15
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2021-12-21 18:08:27 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Enums_Concept
+{
+    class Program
+    {
+        public enum ProductCodes
+        {
+            Milk = 0, 
+            Juice = 1, 
+            Tea = 2 
+          // Constantes
+        }
+        static void Main(string[] args)
+        {
+            ProductCodes test = ProductCodes.Milk;
+            Console.WriteLine((int)test); // 0
+
+            int test2 = 1;
+            Console.WriteLine((ProductCodes)test2); // Juice
+
+            Console.WriteLine(test.ToString()); // Milk
+
+            string test3 = "Tea";
+            ProductCodes getParse;
+
+            bool checkParse = Enum.TryParse(test3, out getParse);
+            Console.WriteLine(getParse); // Tea
+            Console.WriteLine(checkParse); // True
+        }
+    }
+}
